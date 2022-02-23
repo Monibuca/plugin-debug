fa2da696859873b7d498b641d076c6b4d782e695
dexter <178529795@qq.com>
2022-02-23 17:30:03 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/Monibuca/plugin-debug/v4
+
+go 1.18
+
+require (
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	github.com/google/pprof v0.0.0-20220218203455-0368bd9e19a7 // indirect
+	github.com/ianlancetaylor/demangle v0.0.0-20210905161508-09a460cdf81d // indirect
+	golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
+)
---
+++ main.go
@@ -0,0 +1,64 @@
+package debug
+
+import (
+	"io"
+	"net/http"
+	"net/http/pprof"
+	"os"
+	"path/filepath"
+	"strings"
+
+	. "github.com/Monibuca/engine/v4"
+	_ "github.com/mkevac/debugcharts"
+	"go.uber.org/zap"
+)
+
+type DebugConfig struct {
+}
+
+type WriteToFile struct {
+	header http.Header
+	io.Writer
+}
+
+func (w *WriteToFile) Header() http.Header {
+	// return w.w.Header()
+	return w.header
+}
+
+// func (w *WriteToFile) Write(p []byte) (int, error) {
+// 	// w.w.Write(p)
+// 	return w.Writer.Write(p)
+// }
+func (w *WriteToFile) WriteHeader(statusCode int) {
+	// w.w.WriteHeader(statusCode)
+}
+func (p *DebugConfig) OnEvent(event any) {
+	switch event.(type) {
+	case FirstConfig:
+
+	}
+}
+
+func (p *DebugConfig) Trace(w http.ResponseWriter, r *http.Request) {
+	pprof.Trace(w, r)
+}
+
+func (p *DebugConfig) Profile(w http.ResponseWriter, r *http.Request) {
+	file, err := os.OpenFile("cpu.profile", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		plugin.Error("profile", zap.Error(err))
+	}
+	pprof.Profile(&WriteToFile{make(http.Header), file}, r)
+	file.Close()
+	if r.Host == "" {
+		r.Host = "localhost"
+	}
+	w.Write([]byte(strings.Join([]string{"go", "tool", "pprof", "-http :6060", ExecPath, filepath.Join(ExecDir, "cpu.profile")}, " ")))
+}
+
+func (p *DebugConfig) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	pprof.Index(w, r)
+}
+
+var plugin = InstallPlugin(&DebugConfig{})
